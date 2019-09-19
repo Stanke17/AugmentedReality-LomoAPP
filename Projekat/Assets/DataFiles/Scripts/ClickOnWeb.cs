986404637b5b1abb7dc12eca58f13233b6ead6bd
Stanke17 <Salas.ss94@gmail.com>
2019-09-19 18:00:10 +0200

    Add Lomo Project
    Add project in progress

---
+++ Projekat/Assets/DataFiles/Scripts/ClickOnWeb.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(MeshRenderer))]
+public class ClickOnWeb : MonoBehaviour
+{
+    MeshRenderer mesh;
+   void Awake()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    void OnMouseDown()
+    {
+        if(gameObject.name == "Tricikl")
+        {
+            Application.OpenURL("http://www.tricikl.rs/");
+        }
+        else if(gameObject.name == "Instagram")
+        {
+            Application.OpenURL("https://www.instagram.com/tricikl_shop/");
+        }
+        else if(gameObject.name == "Lomo")
+        {
+            Application.OpenURL("https://shop.lomography.com/en/lomoinstant-automat-playa-jardin");
+        }
+    }
+}
---
+++ Projekat/Assets/DataFiles/Scripts/StartScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class StartScript : MonoBehaviour, ITrackableEventHandler
+{
+    public GameObject video;
+    private TrackableBehaviour mtrackableBehaviour;
+    // Start is called before the first frame update
+    void Start()
+    {
+        video.SetActive(false);
+        mtrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if(mtrackableBehaviour)
+        {
+            mtrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if(newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            video.SetActive(true);
+        }
+        else
+        {
+            video.SetActive(false);
+        }
+    }
+}
